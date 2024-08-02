b7b289b09adae30fe73623197248fe3cb08e4be7
wasabina67 <wasabina67@gmail.com>
2024-08-03 02:15:51 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 function myFunction() {
-  Logger.log('test1');
+  const urls = [
+    'https://example.com/',
+    'https://example.com/',
+    'https://example.com/',
+  ];
+
+  urls.forEach((url) => {
+    try {
+      const resp = UrlFetchApp.fetch(url, { muteHttpExceptions: true });
+      const code = resp.getResponseCode();
+      Logger.log(code);
+    } catch (e) {
+      // e
+    }
+  })
 }
